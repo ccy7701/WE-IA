c41d6f471faf774a2e7ae4373ecd131cb911ccd4
Chiew Cheng Yi <chiewccy1@gmail.com>
2023-11-17 18:44:06 +0800

    Small updates to topnav

---
+++ sitejavascript.js
@@ -0,0 +1,77 @@
+// external JavaScript document (sitejavascript.js)
+
+function adjustTopnav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "topnav";
+    }
+}
+
+function addToTable() {
+    // Get all form values
+	var yr = document.forms["myForm"]["year"].value;
+	var ch = document.forms["myForm"]["challenge"].value;
+	var pl = document.forms["myForm"]["plan"].value;
+	var re = document.forms["myForm"]["remark"].value;
+	var table = document.getElementById("projectable");
+
+    // Validate if year is in the format of YYYY/YYYY
+    var yearPattern = /^\d{4}\/\d{4}$/;
+    if (!yearPattern.test(yr)) {
+        return;
+    }
+
+    //Get current number of rows in table by id=projectable
+	var numRows = table.rows.length;
+			
+	//Validate if year, challenge, and plan are empty
+	if(yr == "" || ch == "" || pl == ""){
+		alert("Required field must be filled out");
+	}
+	else {
+	    //Show confirm box to ask if form values will be 
+		//added to the table id=projectable
+		if(confirm("Add to table?")==true){
+			// Find a <table> element with id="projectable":
+			var table = document.getElementById("projectable");
+
+			// Create an empty <tr> element and add it to the
+			// 1st position of the table:
+			var row = table.insertRow();		
+
+			// Insert new cells (<td> elements) at the 1st 
+			// and 2nd position of the "new" <tr> element:
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+			var cell5 = row.insertCell(4);
+
+			// Add values to the new cells:
+			cell1.innerHTML = numRows;
+			cell2.innerHTML = yr;
+			cell3.innerHTML = ch;
+			cell4.innerHTML = pl;
+			//Remark is optional, so if empty then add white
+			//space to the cell
+			if(re=="") cell5.innerHTML = "&nbsp;";
+				else cell5.innerHTML = re;
+		    }
+        }
+}
+
+// REMOVE THESE FUNCTIONS AND THEIR INVOCATIONS IN THE OTHER FILES ONCE ASSIGNMENT IS COMPLETE
+function changeText(element) {
+    // store the original text content
+    element.setAttribute('data-original-text', element.innerText);
+    // change the text content on hover
+    element.innerText = "NOT DONE";
+}
+
+function resetText(element) {
+    // reset the text content on mouseout
+    element.innerText = element.getAttribute('data-original-text');
+}

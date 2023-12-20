50741f9e72656d21776c85a1b899632ec4fb716f
Chiew Cheng Yi <chiewccy1@gmail.com>
2023-12-20 11:03:25 +0800

    modified CSS at kpimodule.php

--- kpimodule.php
+++ kpimodule.php
@@ -16,9 +16,76 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="sitejavascript.js"></script>
+    <script type="text/javascript">
+        function openTab(evt, content) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+                tabcontent[i].style.opacity = 0;    // set opacity to 0 when hiding
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            var selectedTab = document.getElementById(content);
+            selectedTab.style.display = "block";
+            // Trigger a reflow before changing the opacity to ensure the transition is applied
+            selectedTab.offsetHeight;
+            selectedTab.style.opacity = 1; // Set opacity to 1 when displaying
+            evt.currentTarget.className += " active";
+        }
+    </script>
     <style>
         * {
             font-family: Jost, monospace;
+        }
+        main {
+            min-height: 100vh;
+        }
+        .tab-container {
+            padding-left: 5%;
+            padding-right: 5%;
+            box-sizing: border-box;
+        }
+        .tab {
+            overflow: hidden;
+            border: 1px solid #888888;
+            background-color: #f0f0f0;
+        }
+        .tab button {
+            background-color: inherit;
+            float: left;
+            border: none;
+            border-right: 1px solid #888888;
+            outline: none;
+            cursor: pointer;
+            padding-top: 12px;
+            padding-bottom: 12px;
+            transition: 0.1s;
+            font-size: 16px;
+            width: 15%;
+        }
+        .tab button.active {
+            background-color: #333333;
+            color: white;
+        }
+        .tab button:hover {
+            background-color: #666666;
+            color: white;
+        }
+        .tabcontent {
+            display: none;
+            padding: 6px 10px;
+            transition: opacity 0.2s ease;
+            width: 100%;
+            box-sizing: border-box;
+        }
+        #content-container {
+            padding-left: 5%;
+            padding-right: 5%;
+            width: 100%;
+            box-sizing: border-box;
         }
         .indicatorsContainer {
             display: flex;
@@ -60,6 +127,14 @@
             height: 30px;
         }
         @media screen and (max-width: 600px) {
+            .tab button {
+                width: 25%;
+            }
+            #content-container {
+                padding-left: 2%;
+                padding-right: 2%;
+                overflow-x: auto;
+            }
             .attributeCell {
                 width: 40%;
             }
@@ -84,35 +159,46 @@
         <a href="javascript:void(0);" class="icon" onClick="adjustTopnav()"><i class="fa fa-bars"></i></a>
     </nav>
     <main>
-        <div class="indicatorsContainer">
-            <br>
+        <br>
+        <div class="tab-container">
+            <div class="tab">
+                <button class="tablinks" onClick="openTab(event, 'Year1')">Year 1</button>
+                <button class="tablinks" onClick="openTab(event, 'Year2')">Year 2</button>
+                <button class="tablinks" onClick="openTab(event, 'Year3')">Year 3</button>
+                <button class="tablinks" onClick="openTab(event, 'Year4')">Year 4</button>
+            </div>
+        </div>
 
+        <br>
+
+        <div id="Year1" class="tabcontent">
+            <div class="indicatorsContainer">
                 <div class="indicatorBlock">
-                <?php
-                        if (isset($_SESSION["UID"])) {
-                            $accountID = $_SESSION["UID"];
+                    <?php
+                            if (isset($_SESSION["UID"])) {
+                                $accountID = $_SESSION["UID"];
 
-                            // first, fetch data for CGPA, leadership, graduate aim, professional cert, employability and mobility program
-                            $fetchIndicatorQuery = "SELECT * FROM indicator WHERE accountID='".$accountID."' AND indicatorSem=1 AND indicatorYear=1;";
-                            $result = mysqli_query($conn, $fetchIndicatorQuery);
-                            $row = mysqli_fetch_assoc($result);
-                            $indicatorID = $row["indicatorID"];
+                                // first, fetch data for CGPA, leadership, graduate aim, professional cert, employability and mobility program
+                                $fetchIndicatorQuery = "SELECT * FROM indicator WHERE accountID='".$accountID."' AND indicatorSem=1 AND indicatorYear=1;";
+                                $result = mysqli_query($conn, $fetchIndicatorQuery);
+                                $row = mysqli_fetch_assoc($result);
+                                $indicatorID = $row["indicatorID"];
 
-                            // then, fetch from the 'activity' table the number of activites where activityType = 1 OR 2 OR 3
-                            $fetchActivitiesQuery = "SELECT COUNT(*) FROM activity WHERE accountID='".$accountID."' AND activitySem=1 AND activityYear=1 AND activityType IN (1, 2, 3);";
-                            $activitiesResult = mysqli_query($conn, $fetchActivitiesQuery);
-                            $activitiesRow = mysqli_fetch_assoc($activitiesResult);
-                            $activitiesCount = $activitiesRow["COUNT(*)"];
+                                // then, fetch from the 'activity' table the number of activites where activityType = 1 OR 2 OR 3
+                                $fetchActivitiesQuery = "SELECT COUNT(*) FROM activity WHERE accountID='".$accountID."' AND activitySem=1 AND activityYear=1 AND activityType IN (1, 2, 3);";
+                                $activitiesResult = mysqli_query($conn, $fetchActivitiesQuery);
+                                $activitiesRow = mysqli_fetch_assoc($activitiesResult);
+                                $activitiesCount = $activitiesRow["COUNT(*)"];
 
-                            // then, fetch from the 'activity' table the number of competitions where activityType = 4
-                            $fetchCompetitionsQuery = "SELECT COUNT(*) FROM activity WHERE accountID='".$accountID."' AND activitySem=1 AND activityYear=1 AND activityType=4;";
-                            $competitionsResult = mysqli_query($conn, $fetchCompetitionsQuery);
-                            $competitionsRow = mysqli_fetch_assoc($competitionsResult);
-                            $competitionsCount = $competitionsRow["COUNT(*)"];
-                        }
-                        else {
-                            "ERROR: ".mysqli_error($conn);
-                        }
+                                // then, fetch from the 'activity' table the number of competitions where activityType = 4
+                                $fetchCompetitionsQuery = "SELECT COUNT(*) FROM activity WHERE accountID='".$accountID."' AND activitySem=1 AND activityYear=1 AND activityType=4;";
+                                $competitionsResult = mysqli_query($conn, $fetchCompetitionsQuery);
+                                $competitionsRow = mysqli_fetch_assoc($competitionsResult);
+                                $competitionsCount = $competitionsRow["COUNT(*)"];
+                            }
+                            else {
+                                "ERROR: ".mysqli_error($conn);
+                            }
                     ?>
                     <div class="indicatorTopbar">
                         <h4 style="margin: 0;">Semester 1 Year 1</h4>
@@ -129,11 +215,9 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
-
                 <br>
-
                 <div class="indicatorBlock">
-                <?php
+                    <?php
                         if (isset($_SESSION["UID"])) {
                             $accountID = $_SESSION["UID"];
 
@@ -174,11 +258,14 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
+            </div>
+            <br>
+        </div>
 
-                <br>
-
+        <div id="Year2" class="tabcontent">
+            <div class="indicatorsContainer">
                 <div class="indicatorBlock">
-                <?php
+                    <?php
                         if (isset($_SESSION["UID"])) {
                             $accountID = $_SESSION["UID"];
 
@@ -219,9 +306,7 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
-
                 <br>
-
                 <div class="indicatorBlock">
                 <?php
                         if (isset($_SESSION["UID"])) {
@@ -264,11 +349,14 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
+            </div>
+            <br>
+        </div>
 
-                <br>
-
+        <div id="Year3" class="tabcontent">
+            <div class="indicatorsContainer">
                 <div class="indicatorBlock">
-                <?php
+                    <?php
                         if (isset($_SESSION["UID"])) {
                             $accountID = $_SESSION["UID"];
 
@@ -309,9 +397,7 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
-
                 <br>
-
                 <div class="indicatorBlock">
                 <?php
                         if (isset($_SESSION["UID"])) {
@@ -354,11 +440,14 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
+            </div>
+            <br>
+        </div>
 
-                <br>
-
+        <div id="Year4" class="tabcontent">
+            <div class="indicatorsContainer">
                 <div class="indicatorBlock">
-                <?php
+                    <?php
                         if (isset($_SESSION["UID"])) {
                             $accountID = $_SESSION["UID"];
 
@@ -399,9 +488,7 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
-
                 <br>
-
                 <div class="indicatorBlock">
                 <?php
                         if (isset($_SESSION["UID"])) {
@@ -444,13 +531,12 @@
                         <tr><td><b>Mobility Program</b></td><td><?=$row["indicatorMobProg"];?></td></tr>
                     </table>
                 </div>
-
-                <br>
+            </div>
             <br>
-            <?php
-                mysqli_close($conn);
-            ?>
         </div>
+        <?php
+                mysqli_close($conn);
+        ?>
     </main>
     <footer>
         <h5>© Chiew Cheng Yi | BI21110236 | KK34703 Individual Project</h5>
